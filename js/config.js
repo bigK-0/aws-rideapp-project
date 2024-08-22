614c020a5fb55b240aa73cb7b6c33ee41e9729cd
Panachai <92265324+bigK-0@users.noreply.github.com>
2024-08-22 22:05:37 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-southeast-1_6OANIFJbQ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1p18avf5sclfdlujrd18dja0mo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
